945531804d44643e232dd3d3dc23a8204e931317
Felipe Ferreira <felipeben.ferreira@gmail.com>
2021-07-09 17:32:12 -0300

    versao atualizada do js

---
+++ comments.js
@@ -0,0 +1,110 @@
+//criado para salvar todo meu rascunho de js
+console.log('bem vindo ao starter')
+//é assim que se bota comentarios no javascript
+
+
+console.log('Felipe Ferreira')
+console.log(1 + 1)
+console.log(522 / 30)
+console.log(33)
+
+
+/*
+Object
+    objeto
+    propriedades / atributos
+    funcionalidades / metodos 
+
+    {propriedade : "valor" }
+*/
+console.log({
+    name: "felipe",
+    idade: 18,
+    andar: function() {
+        console.log('andar')
+    }
+})
+
+
+/*
+*Array (vetores)
+    *uma lista
+    *agrupamento de dados
+
+*/
+
+console.log([
+    "leite",
+    "ovos",
+
+])
+
+
+//Variaveis
+
+//var 
+/*
+<script> 
+Var clima = "quente"
+clima = "frio"
+console.log(clima)
+</script>
+
+//let é uma variavel constante que nao pode ser mudada
+*/
+
+
+/*
+<script>
+//const e let sao locais e so funcionam no escopo onde foi criada
+
+{
+    let y = 2
+    console.log('> eixste y depois do bloco? ', y)
+     
+}
+
+
+</script>
+*/
+
+
+
+/*
+<script>
+
+var name
+
+name = "felipe"
+
+
+console.log(typeof name) //para ver o tipo de variavel
+let age = 20
+let isHuman = true
+
+//agrupamento de declarações
+console.log(name, age, isHuman)
+
+//escrita de texto + variaveis
+console.log('o ' + name + ' tem ' + age + ' anos')
+//concatenação de variaveis
+
+
+
+
+
+</script>
+*/
+
+//arrays
+const animals = [
+    'Lion',
+    'monkey',
+    'cat'
+   
+   ]
+   
+   //acessar valores dentro do array
+   console.log(animals[0])
+   
+   
--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
-console.log(33)
+//1 
+let weight;
+
+//2 (undefied)
+
+//3
+let name;
+let age;
+let stars;
+let isSubscribed;
+
+name = felipe;
+age = 18;
+stars = 5,3;
+isSubscribed = false;
+
+
+let student = {
+    name = "felipe",
+age = 18,
+stars = 5.3,
+isSubscribed = false
+
+}; //objeto
