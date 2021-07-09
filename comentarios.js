cadd4df68d13b96ca755a344297c418dcfe3a6bd
Felipe Ferreira <felipeben.ferreira@gmail.com>
2021-07-09 14:58:24 -0300

    primeira versao arquivo js

---
+++ comentarios.js
@@ -0,0 +1,8 @@
+//criado para salvar todo meu rascunho de js
+console.log('bem vindo ao starter')
+//é assim que se bota comentarios no javascript
+
+
+console.log('Felipe Ferreira')
+console.log(1 + 1)
+console.log(522 / 30)
